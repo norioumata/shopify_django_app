0895bbd74975fee57c6171af57c1ac4afad25357
Admin <norioumata96727@gmail.com>
2021-01-06 13:53:31 -0500

    Update ALLOWED_HOSTS

--- shopify_django_app/settings.py
+++ shopify_django_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["norio-shopify-django-app.herokuapp.com"]
 
 # Application definition
 
